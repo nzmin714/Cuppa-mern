fd6c38a1aa566eab1a08b76c73420e17319771dd
nzmin714 <minian714@gmail.com>
2022-06-25 10:23:26 +1200

    Initial

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">CuppaT</a>
+      </header>
+      <main>List Product</main>
+    </div>
+  );
+}
+
+export default App;
