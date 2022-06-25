d920a2d91dfa20aebcf60921128e33b10107991f
nzmin714 <minian714@gmail.com>
2022-06-25 14:06:25 +1200

    test redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import HomeScreen from "./Screens/HomeScreen";
+import Product from "./Product/Product";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <a href="/">CuppaT</a>
-      </header>
-      <main>List Product</main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <Link to="/">CuppaT </Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<Product />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
